fc5b83f789c4f900d6744135aa3ccf4181efe520
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-06-12 22:30:12 -0300

    Add receivaAttack function on Gameboard, Fix placeShip

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,4 +1,5 @@
 const Gameboard = () => {
+  const ships = [];
   const cordinates = {
     a: [],
     b: [],
@@ -12,18 +13,51 @@ const Gameboard = () => {
     j: [],
   };
   const placeShip = (y, x, ship) => {
+    const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    const yValue = columns.indexOf(y);
+
+    if (cordinates[y][x - 1] !== undefined) {
+      throw new Error('cannot share the same square between multiple ships');
+    }
+
+    if (ship.isVertical) {
+      if (yValue + ship.length > 10) {
+        throw new Error('Out of range');
+      }
+      for (let i = 0; i < ship.length; i++) {
+        cordinates[columns[yValue + i]][x - 1] = [[`${ship.name}`], [i + 1]];
+      }
+    }
     if (!ship.isVertical) {
       if (x + ship.length > 10) {
         throw new Error('Out of range');
       }
 
       for (let i = 0; i < ship.length; i++) {
-        cordinates[y][x + i - 1] = [[`${ship.name}`], [x + i]];
+        cordinates[y][x + i - 1] = [[`${ship.name}`], [i + 1]];
       }
+    }
+    ships.push(ship);
+  };
+
+  const receiveAttack = (y, x) => {
+    if (cordinates[y][x - 1] === undefined) {
+      cordinates[y][x - 1] = 'missed';
+    } else if (cordinates[y][x - 1] === 'missed') {
+      throw new Error("Can't hit same target multiple times");
+    } else {
+      const shipName = cordinates[y][x - 1][0][0];
+      const shipPlaceHit = cordinates[y][x - 1][1];
+      ships.forEach((ship) => {
+        if (ship.name === shipName) {
+          ship.status[shipPlaceHit - 1] = 'hit';
+          cordinates[y][x - 1] = 'hit';
+        }
+      });
     }
   };
 
-  return { cordinates, placeShip };
+  return { ships, cordinates, placeShip, receiveAttack };
 };
 
 export { Gameboard };
--- src/factories/gameboard.test.js
+++ src/factories/gameboard.test.js
@@ -22,7 +22,7 @@ test('gameboard places ship correctly in horizontal position', () => {
   ]);
 });
 
-test("gameboard doesn't allow to place items out of range", () => {
+test("gameboard doesn't allow to place ships out of range horizontally", () => {
   const gameboard2 = Gameboard();
   const ship2 = {
     name: 'submarine',
@@ -30,4 +30,101 @@ test("gameboard doesn't allow to place items out of range", () => {
     isVertical: false,
   };
   expect(() => gameboard2.placeShip('c', 9, ship2)).toThrow('Out of range');
+});
+
+test('gameboard places ship correctly in vertical position', () => {
+  const gameboard3 = Gameboard();
+  const ship3 = {
+    name: 'submarine',
+    length: 3,
+    isVertical: true,
+  };
+  gameboard3.placeShip('c', 3, ship3);
+  expect(gameboard3.cordinates.c).toEqual([
+    undefined,
+    undefined,
+    [['submarine'], [1]],
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ]);
+  expect(gameboard3.cordinates.d).toEqual([
+    undefined,
+    undefined,
+    [['submarine'], [2]],
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ]);
+  expect(gameboard3.cordinates.e).toEqual([
+    undefined,
+    undefined,
+    [['submarine'], [3]],
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ]);
+});
+
+test("gameboard doesn't allow to place ships out of range vertically", () => {
+  const gameboard2 = Gameboard();
+  const ship2 = {
+    name: 'submarine',
+    length: 3,
+    isVertical: true,
+  };
+  expect(() => gameboard2.placeShip('i', 9, ship2)).toThrow('Out of range');
+});
+
+test("gameboard doesn't allow to place multiple ships in the same place", () => {
+  const gameboard3 = Gameboard();
+  const ship2 = {
+    name: 'submarine',
+    length: 3,
+    isVertical: true,
+  };
+  const ship3 = {
+    name: 'cruiser',
+    length: 3,
+    isVertical: true,
+  };
+  gameboard3.placeShip('b', 2, ship2);
+  expect(() => gameboard3.placeShip('d', 2, ship3)).toThrow();
+});
+
+test('gameboard registers correctly an attack in an empty square', () => {
+  const gameboard4 = Gameboard();
+  gameboard4.receiveAttack('b', 7);
+  expect(gameboard4.cordinates.b[6]).toEqual('missed');
+});
+
+test("gameboard doesn't allow to hit the same target multiple times", () => {
+  const gameboard5 = Gameboard();
+  gameboard5.receiveAttack('c', 3);
+  expect(() => gameboard5.receiveAttack('c', 3)).toThrow();
+});
+
+test('gameboard registers correctly a hit in a ship', () => {
+  const gameboard6 = Gameboard();
+  const ship4 = {
+    name: 'submarine',
+    length: 3,
+    isVertical: true,
+    status: ['clear', 'clear', 'clear'],
+  };
+  gameboard6.placeShip('c', 3, ship4);
+  gameboard6.receiveAttack('d', 3);
+  expect(gameboard6.ships[0].status).toEqual(['clear', 'hit', 'clear']);
 });
